2198146f1afd107940f34a62711c77d2f734ef07
vishu26151 <vishu261534@gmail.com>
2025-05-23 12:37:08 +0000

    variab.. datatyp..

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2615
+let accountEmail = "vishal@Gmail.com"
+var accountpassword = "111"
+accountcity = "delhi"
+
+accountEmail = "vs@vs.com"
+accountpassword = "22"
+accountcity = "mumbai"
+
+   
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountcity])
