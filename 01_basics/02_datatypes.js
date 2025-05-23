98269ae477e96c4c1dea48d11c150d4f5a0d43f0
vishu26151 <vishu261534@gmail.com>
2025-05-23 12:50:42 +0000

    02datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"   //treat all js code as newer version 
+// alert (3+3) we are using nodejs not browser 
+
+console.log (3+
+    3)           //code readability should be higer 
+
+
+    // datatypes
+
+    let name = "vishal"   //string
+    let age = 22          //array
+    let loggedin = false // boolean
+    
+    // 1 number => 2 to power 4
+    // 2 bigint 
+    // string => " "
+    // boolean => true/false
+    //null => standard one value
+    // Undefined => 
+    //symbol => unique
+    
+    // object 
+
+    console.log (typeof undefined);
+    console.log (typeof null);
+    console.log (typeof "vishal");
