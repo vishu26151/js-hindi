108311e4147ae9945292286f55aef312146861b6
Vishal Singh <vishu261534@gmail.com>
2025-06-08 11:25:54 +0000

    03_conversiionOperations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,9 @@
+let score = "20ab"
+
+console.log(typeof score);                  
+console.log(typeof(score));
+
+let valueInNumber = Number (score) 
+console.log(typeof valueInNumber);  
+console.log(valueInNumber);                  
+
